a459d5e5bed38d683af1faf59d2b31723e519910
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-27 13:29:27 -0800

    Names array printing out

--- names.rb
+++ names.rb
@@ -4,3 +4,8 @@ c = {:first_name => "KB", :last_name => "Tonel"}
 d = {:first_name => "Mike", :last_name => "Buyco"}
 e = {:first_name => "Diana", :last_name => "Manlulu"}
 names = [a,b,c,d,e]
+
+puts "You have #{names.length} names in the 'names' array"
+names.each do |i|
+	puts "The name is '" + i[:first_name] + " " + i[:last_name] +"'"
+end
