f66534309e14715c4de1f21f67029d2b3a1cb38a
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-27 12:56:40 -0800

    Initial commit

---
+++ names.rb
@@ -0,0 +1,6 @@
+a = {:first_name => "Michael", :last_name => "Choi"}
+b = {:first_name => "John", :last_name => "Supsupin"}
+c = {:first_name => "KB", :last_name => "Tonel"}
+d = {:first_name => "Mike", :last_name => "Buyco"}
+e = {:first_name => "Diana", :last_name => "Manlulu"}
+names = [a,b,c,d,e]
